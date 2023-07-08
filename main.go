e24ccf124028370af4a7ea4d2ed5c863d0524191
Marek Ruttner <marekruttner811@gmail.com>
2023-07-08 23:36:39 +0200

    users, devices

--- main.go
+++ main.go
@@ -12,15 +12,41 @@ type User struct {
 	Password string
 }
 
-var users = []User{
-	{Username: "admin", Password: "password"},
-	{Username: "user", Password: "123456"},
+type Device struct {
+	ID   int
+	Name string
 }
 
+var (
+	users   []User
+	devices []Device
+)
+
 func main() {
+	// Initialize some sample users and devices
+	initializeData()
+
 	http.HandleFunc("/", calendarHandler)
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/admin", adminHandler)
+	http.HandleFunc("/admin/adduser", addUserHandler)
+	http.HandleFunc("/admin/adddevice", addDeviceHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func initializeData() {
+	// Sample users
+	users = []User{
+		{Username: "admin", Password: "password"},
+		{Username: "user", Password: "123456"},
+	}
+
+	// Sample devices
+	devices = []Device{
+		{ID: 1, Name: "Device 1"},
+		{ID: 2, Name: "Device 2"},
+		{ID: 3, Name: "Device 3"},
+	}
 }
 
 func calendarHandler(w http.ResponseWriter, r *http.Request) {
@@ -54,6 +80,63 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		} else {
 			http.Redirect(w, r, "/login", http.StatusSeeOther)
 		}
+	}
+}
+
+func adminHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the user is authenticated as an admin
+	if !isAuthenticated(r) || !isAdmin(r) {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	// Render the admin template
+	renderTemplate(w, "admin.html", devices)
+}
+
+func addUserHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the user is authenticated as an admin
+	if !isAuthenticated(r) || !isAdmin(r) {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if r.Method == "GET" {
+		renderTemplate(w, "add_user.html", nil)
+	} else if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		// Add the new user
+		newUser := User{Username: username, Password: password}
+		users = append(users, newUser)
+
+		// Redirect back to the admin panel
+		http.Redirect(w, r, "/admin", http.StatusSeeOther)
+	}
+}
+
+func addDeviceHandler(w http.ResponseWriter, r *http.Request) {
+	// Check if the user is authenticated as an admin
+	if !isAuthenticated(r) || !isAdmin(r) {
+		http.Redirect(w, r, "/login", http.StatusSeeOther)
+		return
+	}
+
+	if r.Method == "GET" {
+		renderTemplate(w, "add_device.html", nil)
+	} else if r.Method == "POST" {
+		deviceName := r.FormValue("devicename")
+
+		// Generate a unique ID for the new device
+		newDeviceID := len(devices) + 1
+
+		// Add the new device
+		newDevice := Device{ID: newDeviceID, Name: deviceName}
+		devices = append(devices, newDevice)
+
+		// Redirect back to the admin panel
+		http.Redirect(w, r, "/admin", http.StatusSeeOther)
 	}
 }
 
@@ -91,6 +174,14 @@ func renderTemplate(w http.ResponseWriter, templateFile string, data interface{}
 func isAuthenticated(r *http.Request) bool {
 	cookie, err := r.Cookie("authenticated")
 	if err != nil || cookie.Value == "" {
+		return false
+	}
+	return true
+}
+
+func isAdmin(r *http.Request) bool {
+	cookie, err := r.Cookie("authenticated")
+	if err != nil || cookie.Value != "admin" {
 		return false
 	}
 	return true
